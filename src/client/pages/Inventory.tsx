d61e9cec0f8a259e7f4072cec11f30889083f181
Ezika trust chidi <trustezika831@gmail.com>
2026-07-14 10:07:28 +0100

    fix: pass legacy theme option to AgGridReact to suppress css collision warnings

--- src/client/pages/Inventory.tsx
+++ src/client/pages/Inventory.tsx
@@ -220,6 +220,7 @@ export default function Inventory() {
           }}
         >
           <AgGridReact
+            theme="legacy"
             rowData={products}
             columnDefs={columnDefs}
             pagination={true}
